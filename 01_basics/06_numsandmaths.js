c8e95ed8eec646647c66eff9db2c7cf76d3d5a9a
Abhay Singh <64921353+deltaTH@users.noreply.github.com>
2024-01-06 22:47:39 +0000

    maths done

---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(500);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.67
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+
+// _____________________________________________MATHS + - x / _____________________________________________
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(5.67));
+// console.log(Math.floor(5.67));
+// console.log(Math.ceil(5.67));
+// console.log(Math.min(4,6,3,2,8,6));
+// console.log(Math.max(4,6,3,2,8,6));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));
+
+//dice roll
+const min = 1
+const max = 6
+// console.log(Math.floor(Math.random()* (max - min + 1)) + min);
+
+console.log(Math.floor(Math.random() * 6) + 1); // this FUCKING works
